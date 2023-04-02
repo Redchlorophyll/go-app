838dcd7fd3af26b62b3f2c439af660075a349755
Dhonni Ari <dhonni.saputra@hypefast.id>
2023-04-02 14:50:45 +0700

    feat: initialization

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Redchlorophyll/go-app
+
+go 1.20
+
+require github.com/Redchlorophyll/go-hello-module v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_hello_module "github.com/Redchlorophyll/go-hello-module"
+)
+
+func main() {
+	fmt.Println(go_hello_module.SayHello())
+}
